88a8a1a6f8894be8822816dc08efa3a57b3616d1
ryirv <71331298+ryirv@users.noreply.github.com>
2020-11-24 20:04:01 +0000

    server-side app.js
    added a skeleton for our socket.io requests

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const sql_server = require("./MySQL.js");
+//const sql_server = require("./MySQL.js");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static('client'));
 
-var player = {
-	prev_X:700, 
-	prev_Y: 300,
-	pos_X: 700,
-	pos_Y: 300,
-	direction: 90,
-	points: 101,
-	health: 100,
-	username: "Jeff",
-	IPAddress: "localhost"
-};
-
-sql_server.SetUser(player);
-
-player.prev_Y = 111;
-player.prev_X = 222;
-player.pos_Y = 333;
-player.pos_X = 444;
-direction = 180;
-
-sql_server.UpdateUser(player);
-
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
 	  io.emit('chat message', msg);
 	});
-  });
 
+	socket.on('up_key', function(usr){
+
+	});
+
+	socket.on('down_key', function(usr){
+
+	});
+
+	socket.on('right_key', function(usr){
+
+	});
+
+	socket.on('left_key', function(usr){
+
+	});
+  });
 
 http.listen(3000, function() {
 	console.log("Listening on port: 3000!");
