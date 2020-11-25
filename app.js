39fe0b1af564ca26b6c611bab93885aff3056865
ryirv <71331298+ryirv@users.noreply.github.com>
2020-11-25 11:48:21 +0000

    Added some socket.io inputs
    Added a players dictionary. Added a recieve for input info. Also edited chat message socket to allow for username to appear.

--- app.js
+++ app.js
@@ -4,27 +4,45 @@ var path = require('path');
 //const sql_server = require("./MySQL.js");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var jsondb = require('node-json-db');
 
 app.use(express.static('client'));
 
+
+var players = [];
+
 io.on('connection', function(socket){
+
+	socket.on('new_player', function(usr){
+		players[socket.id] = {
+			username: usr.username,
+			x: 700,
+			y: 300,
+			health: 100,
+			points: 0,
+			ip: socket.request.connection.remoteAddress;
+		};
+	});
+
 	socket.on('chat message', function(msg){
-	  io.emit('chat message', msg);
+		var username = players[socket.id].username;
+	  io.emit('chat message', username + ": " + msg);
 	});
 
-	socket.on('up_key', function(usr){
+	socket.on('input info', function(usr){
 
+		if(usr.playerDir == "right"){ // x +=
+			players[socket.id].x += 5;
+		} else if(usr.playerDir == "down"){ //y +=
+			players[socket.id].y += 5;
+		} else if(usr.playerDir == "left"){ // x -=
+			players[socket.id].x -= 5;
+		} else if(usr.up == "up"){ // y -=
+			players[socket.id].y -= 5;
+		}
 	});
 
-	socket.on('down_key', function(usr){
-
-	});
-
-	socket.on('right_key', function(usr){
-
-	});
-
-	socket.on('left_key', function(usr){
+	socket.on('disconnect', function(){
 
 	});
   });
