6d3821da9554bdfb8c2ac41f1c07d84e15be2df2
Cameron Barker <cameronbarkeran@gmail.com>
2020-11-26 14:19:09 +0000

    restart fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 //const sql_server = require("./MySQL.js");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var leaderboard=[];
+var leaderboard = [];
 app.use(express.static('client'));
 
 
@@ -47,8 +47,9 @@ io.on('connection', function (socket) {
 			players[socket.id].y -= 5;
 			players[socket.id].direction = "facingUp";
 		}
-	
-		players[socket.id].stopped = usr.stopped;
+		if (players[socket.id]) {
+			players[socket.id].stopped = usr.stopped;
+		}
 		//io.emit('input info', players[socket.id]);
 	});
 	///////////////////////////////////////////////////////////////////////////
@@ -72,70 +73,73 @@ io.on('connection', function (socket) {
 			players[socket.id].points += 10;
 		}
 	})
-	
-	
-	
-	socket.on('arrow', function(angle){
-		var arrow={};
+
+
+
+	socket.on('arrow', function (angle) {
+		var arrow = {};
 		arrow.x = players[socket.id].x;
 		arrow.y = players[socket.id].y;
 		arrow.angle = angle;
 		arrow.id = socket.id;
 		io.emit('fire', arrow);
 	})
-	
-	socket.on('getId', function(){
-		if (players[socket.id]){
-			socket.emit('returnId',players[socket.id].id);
-	}})
 
-	socket.on('hit', function(){
-		if (players[socket.id]){
+	socket.on('getId', function () {
+		if (players[socket.id]) {
+			socket.emit('returnId', players[socket.id].id);
+		}
+	})
+
+	socket.on('hit', function () {
+		if (players[socket.id]) {
 			players[socket.id].health -= 25;
-			if (players[socket.id].health <= 0){
-				io.emit('dead',players[socket.id]);
+			if (players[socket.id].health <= 0) {
+				io.emit('dead', players[socket.id]);
 				delete players[socket.id];
 			}
-		
-	}})
+
+		}
+	})
 	/////////////////////////////////////////////////////////////
 	socket.on('update', function () {
 		io.emit('updated', players);
 	});
-	
+
 	socket.on('hit block', function (collidePlayer) {
 		if (players[socket.id]) {
 			players[socket.id].x = collidePlayer.x;
 			players[socket.id].y = collidePlayer.y;
 		}
 	});
-	
-	socket.on('dead', function(){
+
+	socket.on('dead', function () {
 		io.emit('user disconnected', players[socket.id]);
 		delete players[socket.id];
 	})
-	
-	socket.on('searchLeaderboard', function(){
-		for ( var i=0; i<leaderboard.length; i++){
-			if (leaderboard[i].id == socket.id){
+
+	socket.on('searchLeaderboard', function () {
+		for (var i = 0; i < leaderboard.length; i++) {
+			if (leaderboard[i].id == socket.id) {
 				var user = {};
-				user.rank =i+1;
-				user.name =leaderboard[i].username;
+				user.rank = i + 1;
+				user.name = leaderboard[i].username;
 				user.points = leaderboard[i].points;
 				socket.emit('leadPos', user);
 			}
-		}})
-	
-	setInterval(function(){
-		leaderboard=[];
+		}
+	})
+
+	setInterval(function () {
+		leaderboard = [];
 		for (var id in players) {
 			leaderboard.push(players[id]);
-			} 
+		}
 		leaderboard.sort(function (a, b) {
 			return b.points - a.points;
 		});
-		io.emit('leaderboard',leaderboard.slice(0,4));
-	},1000/5)
+		io.emit('leaderboard', leaderboard.slice(0, 4));
+	}, 1000 / 5)
 });
 
 http.listen(3000, function () {
